089a44c12e1af21d6fe53df8b2d7bc49d4ccae9f
LakoNikola <312394nafa@gmail.com>
2021-11-21 21:29:06 +0300

    add UISearchBar

--- UITabl StudentLsit/ls10/ViewController.swift
+++ UITabl StudentLsit/ls10/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
                             "Пернацкая Алеся Юрьевна",
                             "Сандова Галина Александровна",
                             "Елисеева Марина Михайловна"
-    ]
+    ].sorted()
     var dataSourceMan = [ "Aртимович Игорь Владимирович",
                          "Богданович Дмитрий Александрович",
                          "Симонов Владислав Дмитриевич",
@@ -28,15 +28,27 @@ class ViewController: UIViewController {
                          "Малишевский Никита Валерьевич",
                          "Матвеенко Сергей Александрови",
                          "Мостовой Алексей Алексеевич",
-                         "Пачковский Михаил Тадеушевич",
-
-    ]
+                         "Пачковский Михаил Тадеушевич"
+    ].sorted()
+    
+    var filteredDataSourceMan:[String] = []
+    var filteredDataSource:[String] = []
+    let searchController = UISearchController(searchResultsController: nil)
+    var searchBarIsEmpty: Bool {
+        guard let text = searchController.searchBar.text else {return false}
+        return text.isEmpty
+    }
+    private var isFiltering: Bool {
+        return searchController.isActive && !searchBarIsEmpty
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        dataSourceWoman.sort()
-        dataSourceMan.sort()
-        
+
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.placeholder = "Search"
+        definesPresentationContext = true
+        navigationItem.searchController = searchController
     }
 }
 
@@ -48,10 +60,15 @@ extension ViewController: UITableViewDataSource {
         }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         switch section {
-        case 0:
-            return dataSourceWoman.count
+        case 0: if isFiltering {
+            return filteredDataSource.count
+        } else
+            { return dataSourceWoman.count }
         case 1:
-        return dataSourceMan.count
+       if isFiltering {
+            return filteredDataSourceMan.count
+        } else
+            { return dataSourceMan.count }
         default:
             break
         }
@@ -62,9 +79,9 @@ extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         switch section {
         case 0:
-            return "Список студентов \(dataSourceWoman.count)"
+            return "Список студентов: \(dataSourceWoman.count)"
         case 1:
-            return "Список студентов \(dataSourceMan.count)"
+            return "Список студентов: \(dataSourceMan.count)"
         default:
             break
         }
@@ -79,15 +96,46 @@ extension ViewController: UITableViewDataSource {
         cell.nameLabel.text = ""
         
         switch indexPath.section{
-        case 0 :
-            cell.nameLabel.text = dataSourceWoman[indexPath.row]
-        case 1:
-            cell.nameLabel.text = dataSourceMan[indexPath.row]
+        case 0 : if isFiltering{
+            cell.nameLabel.text = filteredDataSource[indexPath.row]
+        } else {
+            cell.nameLabel.text = dataSourceWoman[indexPath.row]}
+
+        case 1: if isFiltering{
+            cell.nameLabel.text = filteredDataSourceMan[indexPath.row]
+        } else {
+            cell.nameLabel.text = dataSourceMan[indexPath.row]}
         default:
             break
         }
         return cell
     }
+
     
+}
+extension ViewController: UISearchResultsUpdating {
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        filterDataSourceMan(searchController.searchBar.text!)
+        filterDataSourceWoman(searchController.searchBar.text!)
+    }
+    private func filterDataSourceMan(_ serchText: String){
+        filteredDataSourceMan = dataSourceMan.filter({ filteretString in
+            return filteretString.lowercased().contains(serchText.lowercased())
+        })
+   
+        
+        tableView.reloadData()
+    }
+   
+    private func filterDataSourceWoman(_ serchText: String){
+        filteredDataSource = dataSourceWoman.filter({ filteretString in
+            return filteretString.lowercased().contains(serchText.lowercased())
+        })
+   
+        
+        tableView.reloadData()
+    }
+     
 }
 
